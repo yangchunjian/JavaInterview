d850e2732cd8d07b1f3c02d507f1983bae066ec2
chunjian.yang <chunjian.yang@opay-inc.com>
2022-04-10 19:17:59 +0800

    更新备注

--- javainterview-all/src/main/java/cn/javainterview/JavaInterview.java
+++ javainterview-all/src/main/java/cn/javainterview/JavaInterview.java
@@ -1,11 +1,5 @@
 package cn.javainterview;
 
-/**
- * @author JavaInterview
- * @email 1091938307@qq.com
- * @created 2022-04-10
- * @timed 18:24
- */
 public class JavaInterview {
 
     public static final String AUTHOR = "JavaInterview";
--- javainterview-core/src/main/java/cn/javainterview/core/util/BaseUtil.java
+++ javainterview-core/src/main/java/cn/javainterview/core/util/BaseUtil.java
@@ -1,10 +1,6 @@
 package cn.javainterview.core.util;
 
-/**
- * @author JavaInterview
- * @email 1091938307@qq.com
- * @created 2022-04-10
- * @timed 18:21
- */
+
+
 public class BaseUtil {
 }
