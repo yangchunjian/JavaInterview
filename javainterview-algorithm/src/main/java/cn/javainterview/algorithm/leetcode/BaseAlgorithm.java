8889bd59ea1fcab454afdc14d4b0f3f32b14c522
chunjian.yang <chunjian.yang@opay-inc.com>
2022-04-11 22:05:49 +0800

    基础算法

---
+++ javainterview-algorithm/src/main/java/cn/javainterview/algorithm/leetcode/BaseAlgorithm.java
@@ -0,0 +1,16 @@
+package cn.javainterview.algorithm.leetcode;
+
+/**
+ * @author Yang Chunjian
+ * @email 1091938307@qq.com
+ * @created 2022-04-11
+ * @timed 22:04
+ */
+public class BaseAlgorithm {
+
+    public static void main(String[] args) {
+        //TODO
+
+
+    }
+}
