880a5e7dd9b9f6a20bd2fd6cc5de913a26245a31
chunjian.yang <chunjian.yang@opay-inc.com>
2022-04-26 23:58:17 +0800

    563. 二叉树的坡度

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -163,6 +163,7 @@ module.exports = {
                             "/algorithm/leetcode/leetcode-404.左叶子之和",
                             "/algorithm/leetcode/leetcode-572.另一棵树的子树",
                             "/algorithm/leetcode/leetcode-559.N叉树的最大深度",
+                            "/algorithm/leetcode/leetcode-563.二叉树的坡度",
                         ]
                     },
                     {
