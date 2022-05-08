1745022c1c744658ee9907a4ea0dbe66239ab36a
chunjian.yang <chunjian.yang@opay-inc.com>
2022-05-08 22:16:21 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -231,10 +231,10 @@ module.exports = {
                             "/algorithm/leetcode/leetcode-559.N叉树的最大深度",
                             "/algorithm/leetcode/leetcode-563.二叉树的坡度",
                             "/algorithm/leetcode/leetcode-863.二叉树中所有距离为 K 的结点",
-                            "/algorithm/leetcode/leetcode-872.叶子相似的树",
                             "/algorithm/leetcode/leetcode-889.根据前序和后序遍历构造二叉树",
                             "/algorithm/leetcode/leetcode-951.翻转等价二叉树",
                             "/algorithm/leetcode/leetcode-968.监控二叉树",
+                            "/algorithm/leetcode/leetcode-872.叶子相似的树",
                         ]
                     },
                     {
