5f2d7abe42c071fbcaf91a65ab3df500da177784
yangchunjian <1091938307@qq.com>
2022-06-25 11:04:55 +0800

    deploy

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -454,9 +454,7 @@ module.exports = {
       name: 'JavaInterview.cn', // 必需
       link: 'https://JavaInterview.cn', // 可选的
     },
-    tag:{
-      text: 'java'
-    }
+    tags: '- Java',
   },
 
   // htmlModules // 插入hmtl(广告)模块
