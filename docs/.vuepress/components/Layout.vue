ac71319495540746ed7142ca10d74611acfe9590
yangchunjian <1091938307@qq.com>
2022-07-29 15:57:15 +0800

    不蒜子

---
+++ docs/.vuepress/components/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <ParentLayout />
+    <Footer />
+  </main>
+</template>
+
+<script>
+
+import ParentLayout from "@parent-theme/Layout.vue";
+
+export default {
+  components: {
+    ParentLayout
+  },
+  computed: {
+
+
+  },
+  mounted() {
+
+
+  }
+};
+</script>
+
