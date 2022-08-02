616dec09e423c116ef13dc43a6b3d7744aafe7f8
yangchunjian <1091938307@qq.com>
2022-08-02 19:29:54 +0800

    一条线

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,38 +1,35 @@
 // 插件配置
 module.exports = [
   //一条线插件
-  [
-    'ribbon',
-    {
-      size: 90,   // 默认数据
-      opacity: 0.3,  //  透明度
-      zIndex: -1,   //  层级
-      opt: {
-        // 色带HSL饱和度
-        colorSaturation: "80%",
-        // 色带HSL亮度量
-        colorBrightness: "60%",
-        // 带状颜色不透明度
-        colorAlpha: 0.65,
-        // 在HSL颜色空间中循环显示颜色的速度有多快
-        colorCycleSpeed: 6,
-        // 从哪一侧开始Y轴 (top|min, middle|center, bottom|max, random)
-        verticalPosition: "center",
-        // 到达屏幕另一侧的速度有多快
-        horizontalSpeed: 200,
-        // 在任何给定时间，屏幕上会保留多少条带
-        ribbonCount: 2,
-        // 添加笔划以及色带填充颜色
-        strokeSize: 0,
-        // 通过页面滚动上的因子垂直移动色带
-        parallaxAmount: -0.5,
-        // 随着时间的推移，为每个功能区添加动画效果
-        animateSections: true
-      },
-      ribbonShow: false, //  点击彩带  true显示  false为不显示
-      ribbonAnimationShow: true  // 滑动彩带
+  ["ribbon-animation", {
+    size: 90,   // 默认数据
+    opacity: 0.3,  //  透明度
+    zIndex: -1,   //  层级
+    opt: {
+      // 色带HSL饱和度
+      colorSaturation: "80%",
+      // 色带HSL亮度量
+      colorBrightness: "60%",
+      // 带状颜色不透明度
+      colorAlpha: 0.65,
+      // 在HSL颜色空间中循环显示颜色的速度有多快
+      colorCycleSpeed: 6,
+      // 从哪一侧开始Y轴 (top|min, middle|center, bottom|max, random)
+      verticalPosition: "center",
+      // 到达屏幕另一侧的速度有多快
+      horizontalSpeed: 200,
+      // 在任何给定时间，屏幕上会保留多少条带
+      ribbonCount: 2,
+      // 添加笔划以及色带填充颜色
+      strokeSize: 0,
+      // 通过页面滚动上的因子垂直移动色带
+      parallaxAmount: -0.5,
+      // 随着时间的推移，为每个功能区添加动画效果
+      animateSections: true
     },
-  ],
+    ribbonShow: false, //  点击彩带  true显示  false为不显示
+    ribbonAnimationShow: true  // 滑动彩带
+  }],
   // 本地插件
   // [require('../plugins/read')], // 阅读时间插件
   [
