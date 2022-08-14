3e2d0399e35e43bf6a9dc058e400ef8d708998f7
chunjian.yang <chunjian.yang@opay-inc.com>
2022-08-14 21:50:04 +0800

    865. 具有所有最深节点的最小子树

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -353,6 +353,7 @@ module.exports = [
                     "/algorithm/leetcode/leetcode-752.打开转盘锁",
                     "/algorithm/leetcode/leetcode-753.破解保险箱",
                     "/algorithm/leetcode/leetcode-863.二叉树中所有距离为 K 的结点",
+                    "/algorithm/leetcode/leetcode-865.具有所有最深节点的最小子树",
                     "/algorithm/leetcode/leetcode-889.根据前序和后序遍历构造二叉树",
                     "/algorithm/leetcode/leetcode-951.翻转等价二叉树",
                     "/algorithm/leetcode/leetcode-958.二叉树的完全性检验",
