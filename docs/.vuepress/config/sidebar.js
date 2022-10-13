b12ac64d29e970cf8dc1303b79600dd25163675a
chunjian.yang <chunjian.yang@opay-inc.com>
2022-10-13 23:47:29 +0800

    788. 旋转数字

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -596,6 +596,7 @@ module.exports = [
                     "/algorithm/leetcode/leetcode-785.判断二分图",
                     "/algorithm/leetcode/leetcode-786.第 K 个最小的素数分数",
                     "/algorithm/leetcode/leetcode-787.K 站中转内最便宜的航班",
+                    "/algorithm/leetcode/leetcode-788.旋转数字",
                     "/algorithm/leetcode/leetcode-817.链表组件",
                     "/algorithm/leetcode/leetcode-834.树中距离之和",
                     "/algorithm/leetcode/leetcode-863.二叉树中所有距离为 K 的结点",
