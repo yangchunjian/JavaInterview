9a3bdca19112afc0cc66c76b8bfcf62d9123b09c
chunjian.yang <chunjian.yang@opay-inc.com>
2022-10-16 21:19:23 +0800

    池化

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -611,6 +611,8 @@ module.exports = [
                     "/algorithm/leetcode/leetcode-796.旋转字符串",
                     "/algorithm/leetcode/leetcode-797.所有可能的路径",
                     "/algorithm/leetcode/leetcode-798.得分最高的最小轮调",
+                    "/algorithm/leetcode/leetcode-799.香槟塔",
+                    "/algorithm/leetcode/leetcode-801.使序列递增的最小交换次数",
                     "/algorithm/leetcode/leetcode-817.链表组件",
                     "/algorithm/leetcode/leetcode-834.树中距离之和",
                     "/algorithm/leetcode/leetcode-863.二叉树中所有距离为 K 的结点",
