c436144254933a05c3fe902dbd90ebd0d1c4dff6
chunjian.yang <chunjian.yang@opay-inc.com>
2022-11-28 22:54:46 +0800

    算法

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -836,6 +836,7 @@ module.exports = [
                     "/algorithm/leetcode/leetcode-1035.不相交的线",
                     "/algorithm/leetcode/leetcode-1037.有效的回旋镖",
                     "/algorithm/leetcode/leetcode-1038.从二叉搜索树到更大和树",
+                    "/algorithm/leetcode/leetcode-1039.多边形三角剖分的最低得分",
                     "/algorithm/leetcode/leetcode-1071.字符串的最大公因子",
                     "/algorithm/leetcode/leetcode-1072.按列翻转得到最大值等行数",
                     "/algorithm/leetcode/leetcode-1073.负二进制数相加",
