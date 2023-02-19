e5fcb252e71a8c29d0754556e38c2f6ee4baa342
chunjian.yang <chunjian.yang@opay-inc.com>
2023-02-19 22:00:12 +0800

    算法

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -864,6 +864,7 @@ module.exports = [
                     "/algorithm/leetcode/leetcode-1071.字符串的最大公因子",
                     "/algorithm/leetcode/leetcode-1072.按列翻转得到最大值等行数",
                     "/algorithm/leetcode/leetcode-1073.负二进制数相加",
+                    "/algorithm/leetcode/leetcode-1074.元素和为目标值的子矩阵数量",
                     "/algorithm/leetcode/leetcode-1078.Bigram 分词",
                     "/algorithm/leetcode/leetcode-1079.活字印刷",
                     "/algorithm/leetcode/leetcode-1080.根到叶路径上的不足节点",
