4c3b15ba5c6b16e54080026a9e6788f0d1232ce3
yangchunjian <1091938308@qq.com>
2023-07-15 16:07:26 +0800

    新增

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ const plugins = require('./config/plugins.js');
 const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
+    compact: false,
     theme: 'vdoing', // 使用依赖包主题
     // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
     title: "JavaInterview",
