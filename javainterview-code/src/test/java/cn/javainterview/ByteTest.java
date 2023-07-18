0bc652bc0cada73619914a3fc5344a7bed3d1c03
yangchunjian <1091938307@qq.com>
2023-07-18 13:28:25 +0800

    同步代码

--- javainterview-code/src/test/java/cn/javainterview/ByteTest.java
+++ javainterview-code/src/test/java/cn/javainterview/ByteTest.java
@@ -3,7 +3,9 @@ package cn.javainterview;
 public class ByteTest {
 
     public static void main(String[] args) {
-        Integer x = 1;
-        int y = x;
+        int i = 10;
+        System.out.println(i++);
+        System.out.println(++i);
+
     }
 }
