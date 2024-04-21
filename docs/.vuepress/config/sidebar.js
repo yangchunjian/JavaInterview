c458c6a2a1fa4e797beec153475e6436172853d6
yangchunjian <1091938307@qq.com>
2024-04-21 20:58:11 +0800

    更新

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -708,6 +708,7 @@ module.exports = [
                     "/algorithm/leetcode/861.翻转矩阵后的得分",
                     "/algorithm/leetcode/862.和至少为 K 的最短子数组",
                     "/algorithm/leetcode/863.二叉树中所有距离为 K 的结点",
+                    "/algorithm/leetcode/864.获取所有钥匙的最短路径",
                     "/algorithm/leetcode/865.具有所有最深节点的最小子树",
                     "/algorithm/leetcode/866.回文素数",
                     "/algorithm/leetcode/867.转置矩阵",
